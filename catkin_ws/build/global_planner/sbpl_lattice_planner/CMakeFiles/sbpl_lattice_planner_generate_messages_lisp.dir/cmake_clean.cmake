dd75532006a91793dc9b3371cf891c9d375e4083
Alex Withers <alexwithers2@gmail.com>
2020-09-18 14:55:30 -0400

    ignorning future build and make files as those must be generated on each machine

--- catkin_ws/build/global_planner/sbpl_lattice_planner/CMakeFiles/sbpl_lattice_planner_generate_messages_lisp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sbpl_lattice_planner_generate_messages_lisp"
-  "/home/alex/pit-navigator-utah/catkin_ws/devel/share/common-lisp/ros/sbpl_lattice_planner/msg/SBPLLatticePlannerStats.lisp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sbpl_lattice_planner_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- catkin_ws/devel/share/webots_ros/cmake/webots_ros-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(webots_ros_MSG_INCLUDE_DIRS "/home/alex/pit-navigator-utah/catkin_ws/src/Simulation_Control/webots_ros/msg")
-set(webots_ros_MSG_DEPENDENCIES std_msgs;sensor_msgs)
